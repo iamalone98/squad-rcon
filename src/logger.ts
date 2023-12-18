854f2168f06e9400c6e88f7cfb4cff4dac83ec48
I-Am Alone <iamalone9828@gmail.com>
2023-12-18 04:18:49 +0300

    add logger time

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,13 @@
 import chalk from 'chalk';
+import { format } from 'date-fns';
+
+const getTime = () => format(new Date(), 'd LLL HH:mm:ss');
 
 export const initLogger = (id: number, enabled: boolean) => ({
   log: (...text: string[]) => {
     enabled &&
       console.log(
-        chalk.yellow(`[SquadRcon][${id}]`),
+        chalk.yellow(`[SquadRcon][${id}][${getTime()}]`),
         chalk.green(text),
       );
   },
@@ -12,7 +15,7 @@ export const initLogger = (id: number, enabled: boolean) => ({
   warn: (...text: string[]) => {
     enabled &&
       console.log(
-        chalk.yellow(`[SquadRcon][${id}]`),
+        chalk.yellow(`[SquadRcon][${id}][${getTime()}]`),
         chalk.magenta(text),
       );
   },
@@ -20,7 +23,7 @@ export const initLogger = (id: number, enabled: boolean) => ({
   error: (...text: string[]) => {
     enabled &&
       console.log(
-        chalk.yellow(`[SquadRcon][${id}]`),
+        chalk.yellow(`[SquadRcon][${id}][${getTime()}]`),
         chalk.red(text),
       );
   },
